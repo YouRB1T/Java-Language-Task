3f0801f5c80543266ee67aaf57cbde01e4618334
YouRB1T <96197496+YouRB1T@users.noreply.github.com>
2025-10-10 01:49:33 +0300

    feat(urov) Дописал crud операции для репозитория с сохранением в файл

--- src/main/java/com/phonebook/repository/PersonRepositoryFIle.java
+++ src/main/java/com/phonebook/repository/PersonRepositoryFIle.java
@@ -9,6 +9,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class PersonRepositoryFIle implements PersonRepository{
     private final String FILENAME = "data/db.json";
@@ -46,31 +47,70 @@ public class PersonRepositoryFIle implements PersonRepository{
 
     @Override
     public Person findPersonId(UUID id) {
-        return null;
+        try {
+            return loadPersons().stream().filter(
+                    person -> person.getId().equals(id)).findFirst()
+                    .orElseThrow();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Person findPersonNoId(String lastName, String number) {
-        return null;
+        try {
+            return loadPersons()
+                    .stream().filter(
+                            person -> person.getLastName().equals(lastName)
+                            && person.getNumbers().contains(number)
+                    ).findFirst().orElseThrow();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Person findPerson(String number) {
-        return null;
+        try {
+            return loadPersons().stream().filter(
+                    person -> person.getNumbers().contains(number)
+            ).findFirst().orElseThrow();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<Person> findAllPersons() {
-        return List.of();
+        try {
+            return loadPersons();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void updatePerson(Person person) {
-
+        try {
+            List<Person> updated = loadPersons().stream()
+                    .map(person1 -> person1.getId().equals(person.getId()) ? person : person1)
+                    .collect(Collectors.toList());
+            savePersons(updated);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deletePerson(UUID id) {
-
+        try {
+            savePersons(
+                    loadPersons().stream().filter(
+                            person -> person.getId() != id
+                    ).toList()
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
